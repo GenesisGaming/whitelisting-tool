188adeca68e10da0fc4801ec7a1c6cb7db407d54
PatriciuBogatu <patriciu.bogatu@assist.ro>
2025-02-05 08:47:52 +0200

    CSN-95026 Initial wireframe implementation

--- src/main/webui/src/js/main.js
+++ src/main/webui/src/js/main.js
@@ -3,3 +3,26 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+document.getElementById('add-ips-btn').addEventListener('click', function(event) { 
+    const newIpsTextarea = document.getElementById('new-ips');
+    const currentIpsTextarea = document.getElementById('current-ips');
+    
+    const newIps = newIpsTextarea.value
+      .split('\n')
+      .map(ip => ip.trim())
+      .filter(ip => ip.length > 0);
+  
+    if (newIps.length === 0) return;
+  
+    const currentIps = currentIpsTextarea.value
+      .split('\n')
+      .map(ip => ip.trim())
+      .filter(ip => ip.length > 0);
+  
+    const uniqueIps = [...new Set([...currentIps, ...newIps])];
+    
+    currentIpsTextarea.value = uniqueIps.join('\n');
+    
+    newIpsTextarea.value = '';
+  });
