537c5acb056506c92435d874245a361aa4eb316a
jvandenberg <jvandenberg@theworkshop.com>
2025-02-06 11:03:39 +0100

    CSN-95010: Update openapi.yml to version 0.3 and updated OperatorApiResource accordingly

--- src/main/java/com/genesis/whitelist/OperatorApiResource.java
+++ src/main/java/com/genesis/whitelist/OperatorApiResource.java
@@ -1,8 +1,7 @@
 package com.genesis.whitelist;
-import jakarta.validation.constraints.Size;
+import com.genesis.whitelist.model.AddIpsRequest;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
-import java.util.List;
 import org.jboss.logging.Logger;
 
 @Path("/operator")
@@ -11,9 +10,9 @@ public class OperatorApiResource implements OperatorApi {
     private static final Logger LOG = Logger.getLogger(OperatorApiResource.class);
 
     @Override
-    public Response addIps(String operatorCode, List<@Size(min = 1, max = 45) String> requestBody) {
-        LOG.info("addIps called for operator " + operatorCode + " : " + requestBody);
-        return Response.ok().header("test", "addIps: " + operatorCode).build();
+    public Response addIps(String operatorCode, AddIpsRequest addIpsRequest) {
+        LOG.info("addIps called for operator " + operatorCode + " : " + addIpsRequest);
+        return null;
     }
 
     @Override
@@ -23,9 +22,9 @@ public class OperatorApiResource implements OperatorApi {
     }
 
     @Override
-    public Response getOperatorIpList(String operatorCode) {
-        LOG.info("getOperatorIpList called for operator " + operatorCode);
-        return Response.ok().header("test", "getOperatorIpList: " + operatorCode).build();
+    public Response getOperatorIpList(String operatorCode, String whitelistType) {
+        LOG.info("getOperatorIpList called for operator " + operatorCode + " with whitelist type " + whitelistType);
+        return null;
     }
 
     @Override
