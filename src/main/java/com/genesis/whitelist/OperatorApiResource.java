ee50252735a266f91ebd7559aa0ba325d807ce46
jvandenberg <jvandenberg@theworkshop.com>
2025-02-06 12:51:16 +0100

    CSN-95010: Added mock generator class and added feature flag

--- src/main/java/com/genesis/whitelist/OperatorApiResource.java
+++ src/main/java/com/genesis/whitelist/OperatorApiResource.java
@@ -1,7 +1,11 @@
 package com.genesis.whitelist;
+
 import com.genesis.whitelist.model.AddIpsRequest;
+import com.genesis.whitelist.model.Operator;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 @Path("/operator")
@@ -9,27 +13,62 @@ public class OperatorApiResource implements OperatorApi {
 
     private static final Logger LOG = Logger.getLogger(OperatorApiResource.class);
 
+    @Inject
+    OperatorApiMockGenerator mockGenerator;
+
+    @ConfigProperty(name = "operator-api.use-mock", defaultValue = "false")
+    boolean useMock;
+
     @Override
     public Response addIps(String operatorCode, AddIpsRequest addIpsRequest) {
         LOG.info("addIps called for operator " + operatorCode + " : " + addIpsRequest);
-        return null;
+
+        if (useMock) {
+            LOG.info("Using mock response for addIps");
+            return mockGenerator.mockAddIps(operatorCode, addIpsRequest);
+        }
+
+        // Real implementation logic here
+        return Response.ok().header("test", "addIps : " + operatorCode + " IPs: "
+            + addIpsRequest.getNewIps()).build();
     }
 
     @Override
-    public Response addOperator(com.genesis.whitelist.model.Operator operator) {
+    public Response addOperator(Operator operator) {
         LOG.info("addOperator called for operator " + operator.getCode());
+
+        if (useMock) {
+            LOG.info("Using mock response for addOperator");
+            return mockGenerator.mockAddOperator(operator);
+        }
+
+        // Real implementation logic here
         return Response.ok().header("test", "addOperator: " + operator.getCode()).build();
     }
 
     @Override
     public Response getOperatorIpList(String operatorCode, String whitelistType) {
         LOG.info("getOperatorIpList called for operator " + operatorCode + " with whitelist type " + whitelistType);
-        return null;
+
+        if (useMock) {
+            LOG.info("Using mock response for getOperatorIpList");
+            return mockGenerator.mockGetOperatorIpList(operatorCode, whitelistType);
+        }
+
+        // Real implementation logic here
+        return Response.ok().header("test", "getOperatorIpList").build();
     }
 
     @Override
     public Response getOperators() {
         LOG.info("getOperators called");
+
+        if (useMock) {
+            LOG.info("Using mock response for getOperators");
+            return mockGenerator.mockGetOperators();
+        }
+
+        // Real implementation logic here
         return Response.ok().header("test", "getOperators").build();
     }
 }
